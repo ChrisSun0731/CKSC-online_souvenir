b60574ed2a06d2bf07612c83fd1013e264c79877
ChrisSun0731 <chris20090731@gmail.com>
2025-11-14 14:33:58 +0800

    root

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cksc-online_souvenir/'
+  base: '/CKSC-online_souvenir/'
 })
